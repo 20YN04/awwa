3b4c3a818302f8ab50c1d957696c5c8cb8d25fb9
20YN04 <Yentl.nerinckx@icloud.com>
2026-03-31 23:33:23 +0200

    fix: correct class name in App component and add initial CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <main class="text-amber-600 class-5xl font-bold">Welcome to an awards winning website</main>
+    <main class="text-amber-600 text-5xl font-bold">Welcome to an awards winning website</main>
   )
 }
 
