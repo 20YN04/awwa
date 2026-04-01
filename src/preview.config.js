d9088a3b109434470ce4ad0e014684feebceccce
20YN04 <Yentl.nerinckx@icloud.com>
2026-04-01 21:18:48 +0200

    feat: update hero component structure and add preview configuration

---
+++ src/preview.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Use "esm" if you use import/export, or "cjs" for require
+  moduleType: "esm", 
+  
+  // If you are using a specific framework, it helps to define it here
+  framework: "react" // or "vue", "svelte", etc.
+};
